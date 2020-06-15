823a3a787fe902d0f20986b2d4143326cf727ffc
Clara Lan <clarajylan@gmail.com>
2020-06-15 15:00:18 -0400

    feat:connect to mongoDB altas successfully

---
+++ MongoDB_FullStack_CRUD_Application/router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//import controllers here:
+
+
+
+
+
+
+module.exports = router;
--- MongoDB_FullStack_CRUD_Application/server.js
+++ MongoDB_FullStack_CRUD_Application/server.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+require('./config/database');
+const router = require('./router'); // import router file
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
